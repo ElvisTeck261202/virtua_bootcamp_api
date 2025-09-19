3c3575e448754556fb74d83141475eaf0cc39657
Ángel Uriel González Urrutia <115201033+UriProtocol@users.noreply.github.com>
2025-09-19 02:27:13 -0600

    small fixes in permissions middleware

--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,20 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::delete('user/{uuid}', [UserController::class, 'destroy']);
 });
 
-Route::middleware(['permission:crear posts'])->group(function () {
+Route::middleware(['permission:agregar posts'])->group(function () {
     Route::post('post', [PostController::class, 'store']);
 });
 
-Route::middleware(['permission: consultar posts'])->group(function () {
+Route::middleware(['permission:consultar posts'])->group(function () {
     Route::get('posts', [PostController::class, 'index']);
     Route::get('posts/{user_uuid}', [PostController::class, 'getPostsByUser']);
 });
 
-Route::middleware(['permission: agregar comentarios'])->group(function () {
+Route::middleware(['permission:agregar comentarios'])->group(function () {
     Route::post('comment', [CommentController::class, 'store']);
 });
 
-Route::middleware(['permission: elimnar comentarios'])->group(function () {
+Route::middleware(['permission:eliminar comentarios'])->group(function () {
     Route::delete('comment/{comment_uuid}', [CommentController::class, 'destroy']);
 });
 
