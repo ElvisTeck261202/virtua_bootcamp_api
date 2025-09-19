e47442ac05153df0d87615d5b4a9791a8addbd89
EvisTeck261202 <duran.jonathan.isw@unipolidgo.edu.mx>
2025-09-18 22:53:40 -0600

    feat: add posts seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             PermissionsSeeder::class,
-            TestUserSeeder::class
+            TestUserSeeder::class,
+            PostsSeeder::class
 
         ]);
     }
